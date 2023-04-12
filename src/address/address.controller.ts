63dab45f80f3d146662448aaa12ab27c1830d7f8
João Vitor <joaovitorcfdev@gmail.com>
2023-04-12 12:34:22 -0300

    feat: insert controller address

---
+++ src/address/address.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AddressService } from './address.service';
+import { CreateAddressDTO } from './dto/createAddress.dto';
+import { promises } from 'dns';
+import { AddressEntity } from './entities/address.entity';
+
+@Controller('address')
+export class AddressController {
+  constructor(private readonly addressService: AddressService) { }
+
+  @Post('/:userId')
+  @UsePipes(ValidationPipe)
+  async createAddress(
+    @Body() createAddressDTO: CreateAddressDTO,
+    @Param('userId') userId: number,
+  ): Promise<AddressEntity> {
+    return this.addressService.createAddress(createAddressDTO, userId);
+  }
+}
---
+++ src/address/address.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AddressEntity } from './entities/address.entity';
+import { Repository } from 'typeorm';
+import { CreateAddressDTO } from './dto/createAddress.dto';
+
+@Injectable()
+export class AddressService {
+  constructor(
+    @InjectRepository(AddressEntity)
+    private readonly addressRepository: Repository<AddressEntity>
+  ) { }
+
+  async createAddress(createAddressDTO: CreateAddressDTO, userId: number): Promise<AddressEntity> {
+    return this.addressRepository.save({
+      ...createAddressDTO,
+      userId,
+    });
+  }
+}

78c90b5596406bbda60ba31c6b842f3e98ed9d07
João Vitor <joaovitorcfdev@gmail.com>
2023-04-13 15:57:50 -0300

    feat: create categoryService

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ReturnCategory } from './dto/returnCategory.dto';
+import { CategoryService } from './category.service';
+import { Roles } from 'src/decorators/roles.decorators';
+import { UserType } from 'src/user/enum/userType.enem';
 
+@Roles(UserType.User, UserType.Admin)
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(
+    private readonly categoryService: CategoryService
+  ) { }
+
+  @Get()
+  async findAllCategories(): Promise<ReturnCategory[]> {
+    return (await this.categoryService.findAllCategories()).map(
+      (category) => new ReturnCategory(category)
+    );
+  }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryEntity } from './entities/category.entity';
+import { Repository } from 'typeorm';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private readonly categoryRepository: Repository<CategoryEntity>
+  ) { }
+
+  async findAllCategories(): Promise<CategoryEntity[]> {
+    const categories = this.categoryRepository.find();
+
+    if (!categories || (await categories).length === 0) {
+      throw new NotFoundException('Categories empty');
+    }
+
+    return categories;
+  }
+}

7e1c17a8a135b0197d96a8acc8249980eab90eca
João Vitor <joaovitorcfdev@gmail.com>
2023-04-17 11:34:22 -0300

    feat: create paymentEntity

---
+++ src/payment/entities/paymentCard.entity.ts
@@ -0,0 +1,12 @@
+import {
+  ChildEntity,
+  Column,
+  Entity,
+} from 'typeorm';
+import { PaymentEntity } from './payment.entity';
+
+@ChildEntity()
+export class PaymentCreditCardEntity extends PaymentEntity {
+  @Column({ name: 'amount_payments', nullable: false })
+  amountPayments: number;
+}

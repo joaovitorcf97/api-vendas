880355df424068a872a9a8fcb9ff539a1238ddec
João Vitor <joaovitorcfdev@gmail.com>
2023-04-19 09:57:32 -0300

    feat:  insert connection soap in project

--- src/correios/correios.controller.ts
+++ src/correios/correios.controller.ts
@@ -2,12 +2,18 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { CorreiosService } from './correios.service';
 import { ReturnCepExternalDTO } from './dto/returnCepExternal.dto';
 import { ReturnCepDTO } from './dto/returnCep.dto';
+import { ResponsePriceCorreios } from './dto/responsePriceCorreios';
 
 @Controller('correios')
 export class CorreiosController {
   constructor(
     private readonly correiosService: CorreiosService,
   ) { }
+
+  @Get('/price')
+  async findPriceDelivery(): Promise<ResponsePriceCorreios> {
+    return this.correiosService.findPriceDelivery();
+  }
 
   @Get('/:cep')
   async findAll(@Param('cep') cep: string): Promise<ReturnCepDTO> {
--- src/correios/correios.service.ts
+++ src/correios/correios.service.ts
@@ -1,14 +1,18 @@
 import { HttpService } from '@nestjs/axios';
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { AxiosError, AxiosResponse } from 'axios';
+import { BadRequestException, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { AxiosError } from 'axios';
 import { ReturnCepExternalDTO } from './dto/returnCepExternal.dto';
 import { CityService } from 'src/city/city.service';
 import { ReturnCepDTO } from './dto/returnCep.dto';
 import { CityEntity } from 'src/city/entities/city.entity';
+import { Client } from 'nestjs-soap';
+import { ResponsePriceCorreios } from './dto/responsePriceCorreios';
 
 @Injectable()
 export class CorreiosService {
   constructor(
+    @Inject('SOAP_CORREIOS')
+    private readonly soapClient: Client,
     private readonly httpService: HttpService,
     private readonly cityService: CityService,
   ) { }
@@ -32,5 +36,32 @@ export class CorreiosService {
     ).catch(() => undefined)
 
     return new ReturnCepDTO(returnCep, city?.id, city?.state?.id);
+  }
+
+  async findPriceDelivery(): Promise<ResponsePriceCorreios> {
+    return new Promise((resolve) => {
+      this.soapClient.CalcPrecoPrazo({
+        nCdServico: '40010',
+        sCepOrigem: '22270010',
+        sCepDestino: '89010000',
+        nVlPeso: 2,
+        nCdFormato: 1,
+        nVlComprimento: 30,
+        nVlAltura: 30,
+        nVlLargura: 30,
+        nVlDiametro: 30,
+        nCdEmpresa: '',
+        sDsSenha: '',
+        sCdMaoPropria: 'N',
+        nVlValorDeclarado: 0,
+        sCdAvisoRecebimento: 'N',
+      }, (_, res: ResponsePriceCorreios) => {
+        if (res) {
+          resolve(res);
+        } else {
+          throw new BadRequestException('Error SOAP');
+        }
+      });
+    });
   }
 }
